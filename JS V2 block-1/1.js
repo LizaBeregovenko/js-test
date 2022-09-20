e35db5e4dba7a4107ee3efb1caad55803d642d7c
Elyzaveta Beregovenko <lizakibich@gmail.com>
2022-09-20 15:14:01 +0300

    JS V2 block-1 1.0

---
+++ JS V2 block-1/1.js
@@ -0,0 +1,16 @@
+// Change code below this line
+const productName = 'Droid';
+console.log(productName);
+
+const pricePerItem = 2000;
+console.log(pricePerItem);
+
+// Оголоси дві змінні, productName для назви товару, 
+// і pricePerItem для зберігання ціни за штуку.
+// При оголошенні надай змінним наступні значення:
+// назва - рядок "Droid";
+// ціна за штуку - число 2000.
+// Оголошена змінна productName
+// Значення змінної productName - це рядок "Droid"
+// Оголошена змінна pricePerItem
+// Значення змінної pricePerItem - це число 2000
---
+++ JS V2 block-1/2.js
@@ -0,0 +1,18 @@
+let productName = "Droid";
+let pricePerItem = 2000;
+
+// Change code below this line
+productName = "Repair droid";
+console.log(productName);
+
+pricePerItem = 3500;
+console.log(pricePerItem);
+
+// Ім'я товару змінили на "Repair droid" і збільшили його ціну на 1500 кредитів. Перевизнач значення змінних pricePerItem і productName після їх оголошення.
+
+// Змінна pricePerItem оголошена за допомогою let
+// При оголошенні змінної pricePerItem присвоєно значення — число 2000
+// Змінній pricePerItem присвоєно нове значення, більше за попереднє на 1500
+// Змінна productName оголошена за допомогою let
+// При оголошенні змінної productName присвоєно значення — рядок "Droid"
+// Змінній productName присвоєно нове значення — рядок "Repair droid"
