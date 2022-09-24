5a955df1695d34db2f2cf7c5afc97cd3e8f6e0f7
Elyzaveta Beregovenko <lizakibich@gmail.com>
2022-09-24 22:37:32 +0300

    JS V2 block-2

---
+++ JS V2 block-2/6.js
@@ -0,0 +1,8 @@
+// Виконай перевизначення значення елементів з індексами 1 і 3. Заміни "plum" на "peach", а "orange" на "banana".
+
+// Оголошена змінна fruits
+// Значення змінної fruits - це масив ["apple", "peach", "pear", "banana"]
+
+const fruits = ["apple", "peach", "pear", "banana"];
+
+// Write your code under this line
---
+++ JS V2 block-2/8.js
@@ -0,0 +1,16 @@
+// Оголоси дві зміні:
+
+// Ім'я змінної	Очікуване значення
+// lastElementIndex	Індекс останнього елемента масиву fruits через довжина_масиву - 1
+// lastElement	Значення останнього елемента масиву
+// Оголошена змінна lastElementIndex
+// Значення змінної lastElementIndex - це число 3
+// Оголошена змінна lastElement
+// Значення змінної lastElement - це рядок "banana"
+
+const fruits = ["apple", "peach", "pear", "banana"];
+const lastElementIndex = fruits.length - 1;
+const lastElement = fruits[lastElementIndex];
+console.log(lastElementIndex);
+console.log(fruits[lastElementIndex]);
+// Change code below this line
