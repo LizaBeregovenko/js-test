ff3153595e660768f4e7c7db647b2489f0d396d5
Elyzaveta Beregovenko <lizakibich@gmail.com>
2022-10-13 23:25:26 +0300

    JS V2 block-4

---
+++ JS V2 block-4/36.js
@@ -0,0 +1,11 @@
+// Доповни функцію calculateTotalBalance(users) таким чином, щоб вона рахувала і повертала суму всіх коштів (властивість balance), які зберігають користувачі з масиву users.
+
+// Оголошена змінна calculateTotalBalance
+// Змінній calculateTotalBalance присвоєна стрілочна функція з параметром (users)
+// Для перебирання параметра users використовується метод reduce()
+// Виклик функції із зазначеним масивом користувачів повертає число 20916
+// Виклик функції з випадковими, але валідними аргументами, повертає правильне значення
+
+// Change code below this line
+const calculateTotalBalance = users => users.reduce((acc, user) => acc + user.balance, 0);
+// Change code above this line
---
+++ JS V2 block-4/41.js
@@ -0,0 +1,47 @@
+// Доповни код таким чином, щоб:
+
+// У змінній sortedByAuthorName вийшов масив книг, відсортований за ім'ям автора в алфавітному порядку.
+// У змінній sortedByReversedAuthorName вийшов масив книг, відсортований за ім'ям автора у зворотному алфавітному порядку.
+// У змінній sortedByAscendingRating вийшов масив книг, відсортований за зростанням рейтингу.
+// У змінній sortedByDescentingRating вийшов масив книг, відсортований за спаданням рейтингу.
+// Оголошена змінна books
+// Значення змінної books - це вихідний масив об'єктів книг
+// Оголошена змінна sortedByAuthorName
+// Значення змінної sortedByAuthorName - це масив книг, відсортований за ім'ям автора в алфавітному порядку
+// Оголошена змінна sortedByReversedAuthorName
+// Значення змінної sortedByReversedAuthorName - це масив книг, відсортований за ім'ям автора у зворотному алфавітному порядку
+// Оголошена змінна sortedByAscendingRating
+// Значення змінної sortedByAscendingRating - це масив книг, відсортований за зростанням рейтингу
+// Оголошена змінна sortedByDescentingRating
+// Значення змінної sortedByDescentingRating - це масив книг, відсортований за спаданням рейтингу
+// Для перебирання масиву books використовується метод sort()
+
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    rating: 8.51,
+  },
+  {
+    title: "The Dream of a Ridiculous Man",
+    author: "Fyodor Dostoevsky",
+    rating: 7.75,
+  },
+  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+  { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+];
+// Change code below this line
+
+const sortedByAuthorName = [...books].sort((firstBook, secondBook) =>
+  firstBook.author.localeCompare(secondBook.author));
+const sortedByReversedAuthorName = [...books].sort((firstBook, secondBook) =>
+  secondBook.author.localeCompare(firstBook.author));
+const sortedByAscendingRating = [...books].sort((
+  firstBook, secondBook) => firstBook.rating - secondBook.rating);
+const sortedByDescentingRating = [...books].sort((
+  firstBook, secondBook) => secondBook.rating - firstBook.rating);
