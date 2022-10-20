e7627789e14a9c654050145c148b924e85709a9d
Elyzaveta Beregovenko <lizakibich@gmail.com>
2022-10-20 19:06:31 +0300

    JS V2 block-5

---
+++ JS V2 block-5/18.js
@@ -0,0 +1,30 @@
+// У застосунку потрібен адміністратор з можливістю додавати пошти користувачів у чорний список.
+
+// Оголоси клас Admin, який наслідує від класу User
+// Додай класу Admin публічну статичну властивість AccessLevel (рівень доступу), значення якої — це об'єкт {BASIC: "basic", SUPERUSER: "superuser"}
+// Оголошений клас Admin
+// Клас Admin наслідує від класу User
+// Клас Admin містить публічну статичну властивість AccessLevel
+// Звернення до Admin.AccessLevel.BASIC повертає рядок "basic"
+// Звернення до Admin.AccessLevel.SUPERUSER повертає рядок "superuser"
+
+class User {
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+class Admin extends User {
+  static AccessLevel = {
+    BASIC: 'basic',
+    SUPERUSER: 'superuser',
+  };
+}
+// Change code below this line
---
+++ JS V2 block-5/9.js
@@ -0,0 +1,25 @@
+// Додай класу Car два методи.
+
+// getPrice() - повертає значення властивості price з об'єкта, який буде його викликати.
+// changePrice(newPrice) - оновлює значення властивості price в об'єкта, який буде його викликати на newPrice.
+// В класі Car оголошений метод getPrice
+// Метод getPrice повертає значення властивості price екземпляра класу, який його викликає
+// В класі Car оголошений метод changePrice
+// Метод changePrice змінює значення властивості price екземпляра класу, який його викликає
+
+class Car {
+  constructor({ brand, model, price }) {
+    this.brand = brand;
+    this.model = model;
+    this.price = price;
+  }
+  // Change code below this line
+  getPrice() {
+    return this.price;
+  }
+  changePrice(newPrice) {
+    this.price = newPrice;
+  }
+
+  // Change code above this line
+}
